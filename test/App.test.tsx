c4d1da97763500d018fee2661f171244065691a9
공정훈 <rhdtl78@gmail.com>
2020-03-28 13:52:36 +0900

    테스트 케이스 제목 변경

--- test/App.test.tsx
+++ test/App.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { mount } from 'enzyme';
 import App from '../src/App';
 
-describe('<Profile />', () => {
+describe('App', () => {
   it('matches snapshot', () => {
     const wrapper = mount(<App />);
     expect(wrapper).toMatchSnapshot();
