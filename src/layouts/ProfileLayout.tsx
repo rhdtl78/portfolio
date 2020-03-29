fe2031dc56771917db9e27d4a209e657c6221197
공정훈 <rhdtl78@gmail.com>
2020-03-29 15:44:24 +0900

    설정 모두 제거
    - 다시 설정

--- src/layouts/ProfileLayout.tsx
+++ src/layouts/ProfileLayout.tsx
@@ -1,21 +1,32 @@
-import React from 'react';
-import { Container, Row } from 'reactstrap';
+import * as React from 'react';
+import { Container, Row, Col } from 'reactstrap';
 
-interface ProfileLayoutProps {
-  image: string;
-  name: string;
-  email: string;
-  instagram: string;
-  github: string;
-}
-
-const ProfileLayout: React.FC<ProfileLayoutProps> = (props) => {
-  const profile = props;
-  return (
-    <Container>
-      <Row></Row>
+const ProfileLayout: React.FC = () => (
+  <div className="bg-secondary flex-fill d-flex align-items-center">
+    <Container fluid className="p-0">
+      <Row noGutters>
+        <Col className="d-flex p-1 justify-content-center">
+          <div className="bg-success" style={{ width: 100, height: 100 }}>
+            Profile 사진
+          </div>
+        </Col>
+      </Row>
+      <Row noGutters>
+        <Col className="d-flex p-1 justify-content-center">
+          <div className="bg-success flex-fill" style={{ height: 100 }}>
+            자기 소개
+          </div>
+        </Col>
+      </Row>
+      <Row noGutters>
+        <Col className="d-flex p-1 justify-content-center">
+          <div className="bg-success flex-fill" style={{ height: 100 }}>
+            링크들
+          </div>
+        </Col>
+      </Row>
     </Container>
-  );
-};
+  </div>
+);
 
 export default ProfileLayout;
